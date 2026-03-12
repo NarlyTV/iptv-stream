557c16b98234fc907609ea5a4aed49acc0bfa9d9
namandhakad712 <namandhakad712@gmail.com>
2026-03-12 23:37:21 +0530

    build(config): set base path to empty string for relative asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/iptv-stream/' : '/',
+  base: '', // Empty string makes all assets relative. Universal for Vercel, Cloudflare, AND Github Pages!
   plugins: [
     react(),
     VitePWA({
